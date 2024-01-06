c88a006f1857a5578ff1f13bdd24d1e2ad1d7476
PashaahsaP <ppsairda@yandex.ru>
2024-01-06 17:29:38 +0700

    set header style

--- resources/views/layouts/authHeader.blade.php
+++ resources/views/layouts/authHeader.blade.php
@@ -2,14 +2,14 @@
     <nav class="nav-container">
 
         <div class="inner-nav-flex">
-            <a class="headerSpan" href="">
-                <span>Менеджер задач</span>
+            <a class="headerSpan" href="#">
+                Менеджер задач
             </a>
         </div>
         <div class="inner-nav-flex">
-            <a href=""><span>Задачи</span></a>
-            <a href=""><span>Статусы</span></a>
-            <a href=""><span>Метки</span></a>
+            <a class="middleLinkHeader" href="#">Задачи</a>
+            <a class="middleLinkHeader" href="#">Статусы</a>
+            <a class="middleLinkHeader" href="#">Метки</a>
         </div>
         <div class="inner-nav-flex">
             <div >
