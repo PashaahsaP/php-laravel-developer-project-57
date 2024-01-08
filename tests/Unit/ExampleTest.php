beab791862ad19a75be0439bddfc274a78ba47e8
PashaahsaP <ppsairda@yandex.ru>
2024-01-08 17:38:23 +0700

    commit example test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,26 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use PHPUnit\Framework\TestCase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
+    // protected function setUp(): void
+    // {
+    //     parent::setUp();
+
+    //     // Start output buffering.
+    //     ob_start();
+    // }
+    // public function test_the_application_returns_a_successful_response(): void
+    // {
+    //     // $response = $this->get('/');
+
+    //     // $response->assertOk();
+    // }
 }
