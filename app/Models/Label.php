2df7807f8f75a77f9be7f9182cfe82b404a5a54d
PashaahsaP <ppsairda@yandex.ru>
2024-02-02 11:58:52 +0700

    issue #9

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -13,23 +13,23 @@ class Label extends Model
         'name',
         'description',
     ];
-    public static array $colors = ['ff0000', '3366ff', '00b300', 'ff9900', '9933ff', '00ffcc'];
+    // public static array $colors = ['ff0000', '3366ff', '00b300', 'ff9900', '9933ff', '00ffcc'];
 
     public function tasks()
     {
         return $this->belongsToMany('App\Models\Task');
     }
 
-    public static function getColor(int $lastItemIndex)
-    {
-        $len = count(self::$colors);
+    // public static function getColor(int $lastItemIndex)
+    // {
+    //     $len = count(self::$colors);
 
-        if ($lastItemIndex >= $len) {
-            return self::getColor($lastItemIndex - $len);
-        } else if ($lastItemIndex < 0) {
-            return self::getColor($lastItemIndex + $len);
-        } else {
-            return self::$colors[$lastItemIndex];
-        }
-    }
+    //     if ($lastItemIndex >= $len) {
+    //         return self::getColor($lastItemIndex - $len);
+    //     } else if ($lastItemIndex < 0) {
+    //         return self::getColor($lastItemIndex + $len);
+    //     } else {
+    //         return self::$colors[$lastItemIndex];
+    //     }
+    // }
 }
